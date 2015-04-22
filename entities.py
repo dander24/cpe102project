dcda301782cca7040ce455d4db69e80226248934
Daniel Anderson <dander24@calpoly.edu>
2015-04-21 17:23:02 -0700

    Partial refractor
    Bug NOT fixed, also not decoupled

--- entities.py
+++ entities.py
@@ -1,6 +1,8 @@
 import point
 
-class Background:
+
+
+class Entity(object):
    def __init__(self, name, imgs):
       self.name = name
       self.imgs = imgs
@@ -21,18 +23,10 @@ class Background:
    def self_string(self):
       return 'unknown'
 
-class MinerNotFull:
-   def __init__(self, name, resource_limit, position, rate, imgs,
-      animation_rate):
-      self.name = name
+class Non_static(Entity):
+   def __init__(self, name, position, imgs):
+      super(Non_static, self).__init__(name,imgs)
       self.position = position
-      self.rate = rate
-      self.imgs = imgs
-      self.current_img = 0
-      self.resource_limit = resource_limit
-      self.resource_count = 0
-      self.animation_rate = animation_rate
-      self.pending_actions = []
 
    def set_position(self, point):
       self.position = point
@@ -40,29 +34,11 @@ class MinerNotFull:
    def get_position(self):
       return self.position
 
-   def get_images(self):
-      return self.imgs
-
-   def get_image(self):
-      return self.imgs[self.current_img]
-
-   def get_rate(self):
-      return self.rate
-
-   def set_resource_count(self, n):
-      self.resource_count = n
-
-   def get_resource_count(self):
-      return self.resource_count
-
-   def get_resource_limit(self):
-      return self.resource_limit
-
-   def get_name(self):
-      return self.name
-
-   def get_animation_rate(self):
-      return self.animation_rate
+class Actor(Non_static):
+   def __init__(self, name, position, imgs, rate):
+      super(Actor,self).__init__(name, position, imgs)
+      self.rate = rate
+      self.pending_actions = []
 
    def remove_pending_action(self, action):
       self.pending_actions.remove(action)
@@ -76,8 +52,51 @@ class MinerNotFull:
    def clear_pending_actions(self):
       self.pending_actions = []
 
-   def next_image(self):
-      self.current_img = (self.current_img + 1) % len(self.imgs)
+   def get_rate(self):
+      return self.rate
+
+class ActorDist(Actor):
+   def __init__(self, name, rate, position, imgs, resource_distance):
+      super(ActorDist,self).__init__(name, position, imgs, rate)
+      self.resource_distance = resource_distance
+
+   def get_resource_distance(self):
+      return self.resource_distance
+      
+class Miner(Actor):
+   def __init__(self, name, resource_limit, position, rate, imgs, animation_rate):
+      super(Miner,self).__init__(name, position, imgs, rate)
+      self.resource_limit = resource_limit
+      self.resource_count = 0
+      self.animation_rate = animation_rate
+
+   def set_resource_count(self, n):
+      self.resource_count = n
+
+   def get_resource_count(self):
+      return self.resource_count
+
+   def get_resource_limit(self):
+      return self.resource_limit
+
+   def get_animation_rate(self):
+      return self.animation_rate
+   
+   
+   
+
+#########################################################################################################################
+
+
+class Background(Entity):
+   def __init__(self, name, imgs):
+      super(Background,self).__init__(name,imgs)
+
+
+class MinerNotFull(Miner):
+   def __init__(self, name, resource_limit, position, rate, imgs, animation_rate):
+      super(MinerNotFull,self).__init__(name, resource_limit, position, rate, imgs,
+      animation_rate)
 
    def self_string(self):
       return ' '.join(['miner', self.name, str(self.position.x),
@@ -85,113 +104,15 @@ class MinerNotFull:
             str(self.rate), str(self.animation_rate)])
  
 
-class MinerFull:
-   def __init__(self, name, resource_limit, position, rate, imgs,
-      animation_rate):
-      self.name = name
-      self.position = position
-      self.rate = rate
-      self.imgs = imgs
-      self.current_img = 0
-      self.resource_limit = resource_limit
-      self.resource_count = resource_limit
-      self.animation_rate = animation_rate
-      self.pending_actions = []
+class MinerFull(Miner):
+   def __init__(self, name, resource_limit, position, rate, imgs, animation_rate):
+      super(MinerFull,self).__init__(name, resource_limit, position, rate, imgs,
+      animation_rate)
 
-   def set_position(self, point):
-      self.position = point
-
-   def get_position(self):
-      return self.position
-
-   def get_images(self):
-      return self.imgs
-
-   def get_image(self):
-      return self.imgs[self.current_img]
-
-   def get_rate(self):
-      return self.rate
-
-   def set_resource_count(self, n):
-      self.resource_count = n
-
-   def get_resource_count(self):
-      return self.resource_count
-
-   def get_resource_limit(self):
-      return self.resource_limit
-
-   def get_name(self):
-      return self.name
-
-   def get_animation_rate(self):
-      return self.animation_rate
-
-   def remove_pending_action(self, action):
-      self.pending_actions.remove(action)
-
-   def add_pending_action(self, action):
-      self.pending_actions.append(action)
-
-   def get_pending_actions(self):
-      return self.pending_actions
-
-   def clear_pending_actions(self):
-      self.pending_actions = []
-
-   def next_image(self):
-      self.current_img = (self.current_img + 1) % len(self.imgs)
-
-   def self_string(self):
-      return 'unknown'
-
-class Vein:
+class Vein(ActorDist):
    def __init__(self, name, rate, position, imgs, resource_distance=1):
-      self.name = name
-      self.position = position
-      self.rate = rate
-      self.imgs = imgs
-      self.current_img = 0
-      self.resource_distance = resource_distance
-      self.pending_actions = []
-
-   def set_position(self, point):
-      self.position = point
-
-   def get_position(self):
-      return self.position
-
-   def get_images(self):
-      return self.imgs
-
-   def get_image(self):
-      return self.imgs[self.current_img]
-
-   def get_rate(self):
-      return self.rate
-
-   def get_resource_distance(self):
-      return self.resource_distance
-
-   def get_name(self):
-      return self.name
-
-   def remove_pending_action(self, action):
-      self.pending_actions.remove(action)
-
-   def add_pending_action(self, action):
-      self.pending_actions.append(action)
-
-   def get_pending_actions(self):
-      return self.pending_actions
-
-   def clear_pending_actions(self):
-      self.pending_actions = []
-
-   def next_image(self):
-      self.current_img = (self.current_img + 1) % len(self.imgs)
-
+      super(Vein,self).__init__(name, rate, position, imgs, resource_distance)
+      
    def self_string(self):
       return ' '.join(['vein', self.name, str(self.position.x),
          str(self.position.y), str(self.rate),
@@ -422,5 +343,3 @@ class Quake:
 
    def self_string(self):
       return 'unknown'
-
-
